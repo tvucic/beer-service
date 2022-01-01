89c24137295a584b4d13460ff3ec250eb145184b
Tomo <tomo.vucic@gmail.com>
2022-01-01 16:26:50 +0100

    Create Initial Project. Closes #1 issue.

---
+++ src/test/java/com/tomo/brewerymicro/BreweryMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomo.brewerymicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
