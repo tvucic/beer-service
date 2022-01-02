388c3ec052c54e7e7af65629fd6d8f9c335f1981
Tomo <tomo.vucic@gmail.com>
2022-01-02 17:35:17 +0100

    BeerControllerTest added an .gitignore file.

--- src/test/java/com/tomo/brewerymicro/web/controller/BeerControllerTest.java
+++ src/test/java/com/tomo/brewerymicro/web/controller/BeerControllerTest.java
@@ -1,6 +1,5 @@
 package com.tomo.brewerymicro.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tomo.brewerymicro.web.model.BeerDto;
 import org.junit.jupiter.api.Test;
